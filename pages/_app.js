170bf3ac2c03b107e9eca8ce7f8c16b510def4f3
hassan214241 <hassan214241@gmail.com>
2021-08-30 01:39:33 -0700

    try to add custom varabile using ant design

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+
+import React from "react";
+import App from "next/app";
+
+// import "../styles/antd.less";
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return <Component {...pageProps} />;
+  }
 }
 
-export default MyApp
+export default MyApp;
